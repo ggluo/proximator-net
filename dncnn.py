d76a31a9a304efa6a672b1d28d9a1208cfcacbc1
Mengmeng Kuang <kuangmeng@msn.com>
2021-03-27 17:23:09 +0800

    Update_DNCNN

--- dncnn.py
+++ dncnn.py
@@ -9,7 +9,6 @@ tf.disable_eager_execution()
 from tf_slim import arg_scope
 
 class dncnn():
-
     def __init__(self, chns=2, filter_size=[3,3], nr_layers=5, nr_filters=32, is_training=True):
         self.filter_size = filter_size
         self.nr_layers   = nr_layers
@@ -22,17 +21,12 @@ class dncnn():
     def body(self, x, init=False):
         lys = []
         with arg_scope([nn.conv2d], init=init, counters = self.counters, nonlinearity=tf.nn.relu):
-
             # pre-layer
             lys.append(nn.conv2d(x, self.nr_filters, self.filter_size)) 
-
-            #
             for _ in range(self.nr_layers):
                 lys.append(nn.conv2d(lys[-1], self.nr_filters, self.filter_size))
-
             # post-layer
             lys.append(nn.conv2d(lys[-1], self.chns, self.filter_size, nonlinearity=None))
-
             return x - lys[-1]
     
     def forward(self, x, init=False):
