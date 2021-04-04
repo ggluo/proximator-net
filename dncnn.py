6728c9316c8fe589e4c7e4cf8f0654dc34bda43a
Jason <luoguan5@gmail.com>
2021-04-04 12:06:26 +0200

    Update dncnn.py

--- dncnn.py
+++ dncnn.py
@@ -9,23 +9,24 @@ tf.disable_eager_execution()
 from tf_slim import arg_scope
 
 class dncnn():
-    def __init__(self, chns=2, filter_size=[3,3], nr_layers=5, nr_filters=32, is_training=True):
+    def __init__(self, chns=2, filter_size=[3,3], nr_layers=5, nr_filters=32, nr_classes=10):
         self.filter_size = filter_size
         self.nr_layers   = nr_layers
         self.nr_filters  = nr_filters
         self.is_training = is_training
-        self.chns        = chns   
+        self.chns        = chns
+        self.nr_classes  = nr_classes
         self.counters    = {}
-        self.model       = tf.make_template('model', self.forward)
+        self.forward     = tf.make_template('model', self.main)
 
-    def body(self, x, h, nr_classes, nonlinearity, init=False):
+    def body(self, x, h, nonlinearity, init=False):
         lys = []
-        with arg_scope([nn.conv2d], init=init, counters = self.counters):
+        with arg_scope([nn.conv2d, nn.cond_instance_norm_plus], init=init, counters = self.counters):
 
             # pre-layer
             ######
             l_o = nn.conv2d(x, self.nr_filters, self.filter_size, nonlinearity=None))
-            l_o = nn.cond_instance_norm_plus(l_o, h, nr_classes)
+            l_o = nn.cond_instance_norm_plus(l_o, h, self.nr_classes)
             l_o = nonlinearity(l_o)
             ######
             lys.append(l_o)
@@ -36,7 +37,7 @@ class dncnn():
             lys.append(nn.conv2d(lys[-1], self.chns, self.filter_size, nonlinearity=None))
             return x - lys[-1]
     
-    def forward(self, x, h, nr_classes, nonlinearity, init=False):
-        out = self.body(x, h, nr_classes, nonlinearity, init)
+    def main(self, x, h, nonlinearity, init=False):
+        out = self.body(x, h, nonlinearity, init)
         self.counters = {}
         return out
