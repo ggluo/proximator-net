fd52bcedb6506d87194baf6d51033e92fa75ba2c
Jason <luoguan5@gmail.com>
2021-04-04 12:18:41 +0200

    Update dncnn.py

--- dncnn.py
+++ dncnn.py
@@ -9,17 +9,18 @@ tf.disable_eager_execution()
 from tf_slim import arg_scope
 
 class dncnn():
-    def __init__(self, chns=2, filter_size=[3,3], nr_layers=5, nr_filters=32, nr_classes=10):
+    def __init__(self, chns=2, filter_size=[3,3], nr_layers=5, nr_filters=32, nr_classes=10, nonlinearity=tf.nn.relu):
         self.filter_size = filter_size
         self.nr_layers   = nr_layers
         self.nr_filters  = nr_filters
         self.is_training = is_training
         self.chns        = chns
         self.nr_classes  = nr_classes
+        self.nonlinearity= nonlinearity
         self.counters    = {}
         self.forward     = tf.make_template('model', self.main)
 
-    def body(self, x, h, nonlinearity, init=False):
+    def body(self, x, h, init=False):
         lys = []
         with arg_scope([nn.conv2d, nn.cond_instance_norm_plus], init=init, counters = self.counters):
 
@@ -37,7 +38,7 @@ class dncnn():
             lys.append(nn.conv2d(lys[-1], self.chns, self.filter_size, nonlinearity=None))
             return x - lys[-1]
     
-    def main(self, x, h, nonlinearity, init=False):
-        out = self.body(x, h, nonlinearity, init)
+    def main(self, x, h, init=False):
+        out = self.body(x, h, init)
         self.counters = {}
         return out
